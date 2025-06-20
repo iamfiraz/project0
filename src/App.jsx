2ca0ad94de1a4638dfa53ebf2a34ab7c37e59d59
iamfiraz <firazm666@gmail.com>
2025-06-20 16:10:55 +0530

    added photo

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './KTUConnect1.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<img src="KTUConnect1.png" alt="KTU Connect Logo" />
+
     </>
   )
 }
