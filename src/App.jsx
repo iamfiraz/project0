3133691731fb4f7515e59389573e88c2bfb53fc7
unknown <you@example.com>
2025-06-21 01:19:51 +0530

    addedphoto

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,6 @@ function App() {
          <Navbar />
       <Routes>
         <Route path='/l' element={<Login />} />
-        <Route path='/s' element={<Signup />} />
-        <Route path='/n' element={<Name />} />
-        <Route path='/c' element={<Counter />} />
-        <Route path='/ch' element={<Change />} />
-        <Route path='/u' element={<User />} />
-        <Route path='/p' element={<Product />} />
       </Routes>
     </>
   )
